e4f5595e132f67c2e565179fc6a18c4ed82d0c06
pradashades333 <alexdreve03@gmail.com>
2026-03-27 19:39:51 +0100

    fixed routes

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,29 @@ const router = express.Router();
 const controller = require("../controllers/controller");
 const upload = require("../db/multer");
 
+function ensureAuth(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/log-in");
+}
+
 router.get("/sign-up", controller.signUpGet);
 router.post("/sign-up", controller.signUpPost);
 router.get("/log-in", controller.logInGet);
 router.post("/log-in", controller.logInPost);
 router.get("/log-out", controller.logOut);
-router.post("/files", upload.single("file"), controller.uploadFile);
+router.post("/files", ensureAuth, upload.single("file"), controller.uploadFile);
 
 
-router.get("/", controller.index);
-router.get("/folders/new", controller.newFolderGet);
-router.post("/folders", controller.newFolderPost);
-router.get("/folders/:id", controller.showFolder);
-router.get("/folders/:id/edit", controller.editFolderGet);
-router.post("/folders/:id/edit", controller.editFolderPost);
-router.post("/folders/:id/delete", controller.deleteFolder);
+router.get("/", ensureAuth, controller.index);
+router.get("/folders/new", ensureAuth, controller.newFolderGet);
+router.post("/folders", ensureAuth, controller.newFolderPost);
+router.get("/folders/:id", ensureAuth, controller.showFolder);
+router.get("/folders/:id/edit", ensureAuth, controller.editFolderGet);
+router.post("/folders/:id/edit", ensureAuth, controller.editFolderPost);
+router.post("/folders/:id/delete", ensureAuth, controller.deleteFolder);
 
-router.get("/files/:id", controller.showFile);
-router.post("/files", controller.uploadFile);
-router.post("/files/:id/delete", controller.deleteFile);
+router.get("/files/:id", ensureAuth, controller.showFile);
+router.post("/files/:id/delete", ensureAuth, controller.deleteFile);
 
 module.exports = router;
 
